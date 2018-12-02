4ef603a7fee158e773b5532f764ff1d418409ba9
aaabidi <ammarabidi99@gmail.com>
2018-12-02 18:50:30 -0500

    ugh

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :food
   belongs_to :user
-  validates :title, presence: exists
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :comments
   validates :password, presence: true
   validates :name, presence: true
   validates :email, presence: true
