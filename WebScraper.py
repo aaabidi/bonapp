a40a27bfed4250162572ac242430f077533ccf07
NickKettler <nickakettler@gmail.com>
2018-12-02 20:04:17 -0500

    Prints all food items to console

--- WebScraper.py
+++ WebScraper.py
@@ -2,32 +2,18 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-from array import array
-stringarray = array('i')
+#fooditems = []
 
 html = urlopen("https://case.cafebonappetit.com/cafe/fribley-marche/")
-print(html)
-
-
-# specify the url
-#quote_page = 'https://case.cafebonappetit.com/cafe/fribley-marche/'
-
-# query the website and return the html to the variable ‘page’
-#page = urllib2.urlopen(quote_page)
 
 # parse the html using beautiful soup and store in variable `soup`
 soup = BeautifulSoup(html, "html.parser")
 
 
-# Take out the <div> of name and get its value
-#name_box = soup.find('h1', attrs={'class': 'name'})
+fooditems = soup.find_all('button',class_='h4 site-panel__daypart-item-title')
 
-#name = name_box.text.strip() # strip() is used to remove starting and trailing
-print (soup.title.string)
-#print (soup.find_all(id='daypart-modal_title--3'))
-food_names = soup.findNext('button',attrs={"class":"h4 site-panel__daypart-item-title"})
-name = food_names.text.strip()
-print (name)
+for x in fooditems:
+    print (x.text.strip())
 
-#print(*i, sep='\n')
-#print (soup.findAll('h2',attrs={"class":"daypart-modal__title"}))
+
+
